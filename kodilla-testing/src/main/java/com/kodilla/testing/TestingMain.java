70e92b7b375cc7186fb3cfa64ae8c4fdaffa7759
pimisiak <p.misiak@gmail.com>
2017-08-02 19:45:48 +0200

    Add SimpleUser and Calculator tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        System.out.println("Test User");
+        if (result.equals("theForumUser")) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int a = 123;
+        int b = 23;
+        System.out.println("Test Calculator Add");
+        if (calculator.add(a, b) == 146) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Test Calculator Substract");
+        if (calculator.substract(a, b) == 100) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
