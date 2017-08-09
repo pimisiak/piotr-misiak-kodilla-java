e050ea236b912390a01c3d7bcf6b979db9dc635b
pimisiak <p.misiak@gmail.com>
2017-08-09 21:40:44 +0200

    Add iterate package

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
@@ -0,0 +1,16 @@
+package com.kodilla.stream.iterate;
+
+import java.util.stream.Stream;
+
+public final class NumbersGenerator {
+    /**
+     * Generate even numbers.
+     * @param max Limits the size of the Stream
+     */
+    public static void generateEven(final int max) {
+        Stream.iterate(1, n -> n + 1)
+                .limit(max)
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+    }
+}
