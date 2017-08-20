be7a4d1827c1fa714bbf0da3ce3a8adb2b65bd4e
pimisiak <p.misiak@gmail.com>
2017-08-20 14:45:25 +0200

    Update exception package

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,10 +1,20 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
 
 public class ExceptionModuleRunner {
+    /**
+     * Reads file with exception handling.
+     *
+     * @param args arguments of main
+     */
     public static void main(final String[] args) {
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        try {
+            fileReader.readFile();
+        } catch (FileReaderException e) {
+            System.out.println("Problem while reading a file");
+        }
     }
 }
