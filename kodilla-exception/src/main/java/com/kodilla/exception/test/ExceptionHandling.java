1c06ea0541d7b27b011ebd284a83e11e1f2c8595
pimisiak <p.misiak@gmail.com>
2017-08-22 00:34:30 +0200

    8.3 add finally block in ExceptionHandling

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -9,10 +9,13 @@ class ExceptionHandling {
     public static void main(final String[] args) {
         //SecondChallenge exception assignment
         final SecondChallenge secondChallenge = new SecondChallenge();
+        String result = "Not done";
         try {
-            System.out.println(secondChallenge.probablyWillThrowException(2, 1));
+            result = secondChallenge.probablyWillThrowException(2, 1);
         } catch (Exception e) {
             System.out.println("Error in method probablyWillThrowException: " + e);
+        } finally {
+            System.out.println("Result: " + result);
         }
 
         //Flight exception assignment
