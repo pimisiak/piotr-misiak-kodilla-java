f543a61a18ff4c1e246d69f14d0d9584d4f02aaa
pimisiak <p.misiak@gmail.com>
2017-08-25 16:11:22 +0200

    9.2 update Application

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/order/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/order/Application.java
@@ -4,23 +4,23 @@ import java.time.LocalDateTime;
 
 final class Application {
     public static void main(final String[] args) {
-        User user = new User.UserBuilder("user123")
+        final User user = new User.UserBuilder("user123")
                 .address("ul Rola 12/13")
                 .firstName("Bartek")
                 .lastName("Bartkowski")
                 .email("bart@bart.com")
                 .build();
-        Order orderForBook = new Order.OrderBuilder(123)
+        final Order orderForBook = new Order.OrderBuilder(123)
                 .user(user)
                 .dateOfOrder(LocalDateTime.of(2017, 8, 22, 12, 55, 15))
                 .shippingAddress(user.getAddress())
                 .build();
-        Product bookProduct = new Product("Book", 12.35);
+        final Product bookProduct = new Product("Book", 12.35);
         bookProduct.addProperty("Title", "Thinking in Java");
         bookProduct.addProperty("Author", "Bruce Eckel");
         orderForBook.addProduct(bookProduct, 1);
 
-        ProductOrderService productOrderService = new ProductOrderService(new BankTransferService(), new MailService(), new ProductOrderRepository());
+        final ProductOrderService productOrderService = new ProductOrderService(new BankTransferService(), new MailService(), new ProductOrderRepository());
         OrderDto orderForBookDto = productOrderService.processOrder(orderForBook);
     }
 }
