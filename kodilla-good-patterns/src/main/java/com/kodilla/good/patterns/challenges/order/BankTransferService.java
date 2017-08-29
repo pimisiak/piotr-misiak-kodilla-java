3102d86ff95a894889b96efb6d219cf7016976c7
pimisiak <p.misiak@gmail.com>
2017-08-30 00:22:38 +0200

    9.2 update challenges.order package

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/order/BankTransferService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/order/BankTransferService.java
@@ -3,7 +3,9 @@ package com.kodilla.good.patterns.challenges.order;
 final class BankTransferService implements PaymentService {
     @Override
     public PaymentResult processPayment(final PaymentRequirements paymentRequirements) {
-        System.out.printf("Processing payment %.2f with bank transfer.%n", paymentRequirements.getPayment());
+        System.out.printf("Processing payment %.2f with bank transfer for account %s.%n",
+                paymentRequirements.getPayment(),
+                paymentRequirements.getUser().getBankAccountNumber());
         return new PaymentResult(true);
     }
 }
