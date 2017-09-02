e0cb5f687ccf8e09388644509dd239e0dbe488c4
pimisiak <p.misiak@gmail.com>
2017-09-02 16:02:54 +0200

    10.3 update spring.portfolio

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.portfolio;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
