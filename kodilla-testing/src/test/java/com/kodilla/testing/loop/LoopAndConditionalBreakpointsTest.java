f3baaec0ce59e3203cc29b8fc48b73fe19969343
Piotr Misiak <p.misiak@gmail.com>
2017-11-29 20:33:27 +0100

    add testing.loop package

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/LoopAndConditionalBreakpointsTest.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing.loop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoopAndConditionalBreakpointsTest {
+    @Test
+    public void testLoop() {
+        //Given
+        long sum = 0;
+        //When
+        for (int n = 0; n < 1000; n++) {
+            sum += n;
+            System.out.println(String.format("[ %d ] sum equals: %d", n, sum));
+        }
+        //Then
+        assertEquals(4995000, sum);
+    }
+}
